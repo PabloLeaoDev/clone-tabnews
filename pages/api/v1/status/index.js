3eb7eeb062aa77c40ad22d62224e382ac72bc3a2
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-01-22 23:01:47 -0300

    add files `database.js` and `.env`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -4,7 +4,7 @@ async function status(request, response) {
   const result = await database.query('SELECT 1 + 1 as sum;');
   console.log(result.rows);
 
-  response.status(200).json({status: 'ok'});
+  return response.status(200).json({status: 'ok'});
 }
 
 export default status;
