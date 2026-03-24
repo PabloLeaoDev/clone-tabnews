c7349254b6a923992f93d5596bd643a30abbca38
PabloLeaoDev <devpablol@gmail.com>
2026-03-24 10:05:49 -0300

    fix `/migrations` endpoint conn error

--- pages/api/v1/migrations/index.js
+++ pages/api/v1/migrations/index.js
@@ -1,6 +1,7 @@
 import migrationRunner from "node-pg-migrate";
 import { join } from "node:path";
 import database from "infra/database";
+import { error } from "node:console";
 
 async function migrations(request, response) {
   const migrationsType = {
@@ -8,33 +9,37 @@ async function migrations(request, response) {
     applied: 200,
   };
 
-  let 
-    dryRun = true,
-    dbClient = await database.getNewClient();
-
-  if (request.method === "GET") 
-    console.log("GET:");
-  else if (request.method === "POST") {
-    console.log("POST:");
-    dryRun = false;
-  } else return response.status(405).json({ message: "Method not allowed" });
-
-  const migrations = await migrationRunner({
-    dir: join("infra", "migrations"),
-    direction: "up",
-    verbose: true,  
-    migrationsTable: "pgmigrations",
-    dryRun,
-    dbClient,
-  });
-
-  await dbClient.end();
-
-  return response
+  try {
+    let 
+      dryRun = true,
+      dbClient = await database.getNewClient();
+  
+    if (request.method === "GET") 
+      console.log("GET:");
+    else if (request.method === "POST") {
+      console.log("POST:");
+      dryRun = false;
+    } else return response.status(405).json({ message: "Method not allowed" });
+  
+    const migrations = await migrationRunner({
+      dir: join("infra", "migrations"),
+      direction: "up",
+      verbose: true,  
+      migrationsTable: "pgmigrations",
+      dryRun,
+      dbClient,
+    });
+    
+    return response
     .status(
       migrations.length > 0 ? migrationsType.pending : migrationsType.applied,
     )
     .json(migrations);
+  } catch (e) {
+    console.error(error.message);
+  } finally {
+    await dbClient.end();
+  }
 }
 
 export default migrations;
