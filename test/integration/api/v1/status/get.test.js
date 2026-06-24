ebb7b3bf061cd43bfba3cd551d85099646cc0ff9
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-06-24 11:11:51 -0300

    test: rename all descriptions to use new pattern

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -4,38 +4,46 @@ beforeAll(async () => {
   await orchestrator.waitForAllServices();
 });
 
-test("GET api/v1/status should return 200", async () => {
-  const response = await fetch("http://localhost:3000/api/v1/status");
-  // expect(response.status).toBe(200);
+describe("GET api/v1/status", () => {
+  describe("Anonymous user", () => {
+    test("Retrieving current system status", async () => {
+      const response = await fetch("http://localhost:3000/api/v1/status");
+      // expect(response.status).toBe(200);
 
-  const responseBody = await response.json();
+      const responseBody = await response.json();
 
-  expect(responseBody.updated_at).toBeDefined();
-  expect(responseBody.dependencies.database).toBeDefined();
-  expect(responseBody.dependencies.database).toHaveProperty("environment");
-  expect(responseBody.dependencies.database).toHaveProperty("server_version");
-  expect(responseBody.dependencies.database).toHaveProperty("max_connections");
-  expect(responseBody.dependencies.database).toHaveProperty(
-    "opened_connections",
-  );
+      expect(responseBody.updated_at).toBeDefined();
+      expect(responseBody.dependencies.database).toBeDefined();
+      expect(responseBody.dependencies.database).toHaveProperty("environment");
+      expect(responseBody.dependencies.database).toHaveProperty(
+        "server_version",
+      );
+      expect(responseBody.dependencies.database).toHaveProperty(
+        "max_connections",
+      );
+      expect(responseBody.dependencies.database).toHaveProperty(
+        "opened_connections",
+      );
 
-  console.log(responseBody);
+      console.log(responseBody);
 
-  const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
+      const parsedUpdatedAt = new Date(responseBody.updated_at).toISOString();
 
-  expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
-  // expect(isNaN(responseBody.dependencies.database.server_version)).toBe(false);
-  expect(responseBody.dependencies.database.server_version.split(".")[0]).toBe(
-    "16",
-  );
-  expect(
-    Number.isInteger(
-      Number(responseBody.dependencies.database.max_connections),
-    ),
-  ).toBe(true);
-  expect(
-    Number.isInteger(
-      Number(responseBody.dependencies.database.opened_connections),
-    ),
-  ).toBe(true);
+      expect(responseBody.updated_at).toEqual(parsedUpdatedAt);
+      // expect(isNaN(responseBody.dependencies.database.server_version)).toBe(false);
+      expect(
+        responseBody.dependencies.database.server_version.split(".")[0],
+      ).toBe("16");
+      expect(
+        Number.isInteger(
+          Number(responseBody.dependencies.database.max_connections),
+        ),
+      ).toBe(true);
+      expect(
+        Number.isInteger(
+          Number(responseBody.dependencies.database.opened_connections),
+        ),
+      ).toBe(true);
+    });
+  });
 });
