4d1b33e59d291661b8465b3ee9382f7ca5fa3553
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-06-27 20:27:54 -0300

    feat: create and use `InternalServerError` in `/api/v1/status`

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,25 +1,36 @@
 import database from "infra/database.js";
+import { InternalServerError } from "infra/errors.js";
 
 async function status(request, response) {
-  const updatedAt = new Date().toISOString(),
-    appEnv = process.env.NODE_ENV || "unknown",
-    resStatus = await database.query(`
-          SELECT
-            current_setting('server_version') as server_version,
-            current_setting('max_connections') as max_connections,
-            count(*)::int as opened_connections
-          FROM pg_stat_activity
-        `);
+  try {
+    const updatedAt = new Date().toISOString(),
+      appEnv = process.env.NODE_ENV || "unknown",
+      resStatus = await database.query(`
+            SELECT
+              current_setting('server_version') as server_version,
+              current_setting('max_connections') as max_connections,
+              count(*)::int as opened_connections
+            FROM pg_stat_activity
+          `);
 
-  return response.status(200).json({
-    updated_at: updatedAt,
-    dependencies: {
-      database: {
-        environment: appEnv,
-        ...resStatus.rows[0],
+    return response.status(200).json({
+      updated_at: updatedAt,
+      dependencies: {
+        database: {
+          environment: appEnv,
+          ...resStatus.rows[0],
+        },
       },
-    },
-  });
+    });
+  } catch (error) {
+    const publicErrorObject = new InternalServerError({
+      cause: error,
+    });
+    console.error(publicErrorObject);
+    return response
+      .status(publicErrorObject.statusCode)
+      .json(publicErrorObject);
+  }
 }
 
 export default status;
