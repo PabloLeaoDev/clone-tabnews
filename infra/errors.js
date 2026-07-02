588d73b4d07760726b8cfcce639981a83aea8178
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-07-02 11:06:38 -0300

    feat: adds `MethodNotAllowedError` and `ServiceError`

--- infra/errors.js
+++ infra/errors.js
@@ -1,9 +1,45 @@
 export class InternalServerError extends Error {
-  constructor({ cause }) {
+  constructor({ cause, statusCode }) {
     super("A not handled error occurred", { cause });
     this.name = "InternalServerError";
     this.action = "Contact the support team";
-    this.statusCode = 500;
+    this.statusCode = statusCode || 500;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class ServiceError extends Error {
+  constructor({ cause, message }) {
+    super(message || "Service unavailable in this moment", { cause });
+    this.name = "ServiceError";
+    this.action = "Verify if the service is available";
+    this.statusCode = 503;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
+export class MethodNotAllowedError extends Error {
+  constructor() {
+    super("A not allowed method was used on this endpoint");
+    this.name = "MethodNotAllowedError";
+    this.action = "Verify your request method is valid for this endpoint";
+    this.statusCode = 405;
   }
 
   toJSON() {
