92ceb9fc73368f98046a6d7e576e8a2ab48b4045
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-07-18 18:27:36 -0300

    feat: adds `ValidationError` error

--- infra/errors.js
+++ infra/errors.js
@@ -34,6 +34,24 @@ export class ServiceError extends Error {
   }
 }
 
+export class ValidationError extends Error {
+  constructor({ cause, message, action }) {
+    super(message || "A validation error occurred", { cause });
+    this.name = "ValidationError";
+    this.action = action || "Verify sent data and try again";
+    this.statusCode = 400;
+  }
+
+  toJSON() {
+    return {
+      name: this.name,
+      message: this.message,
+      action: this.action,
+      status_code: this.statusCode,
+    };
+  }
+}
+
 export class MethodNotAllowedError extends Error {
   constructor() {
     super("A not allowed method was used on this endpoint");
