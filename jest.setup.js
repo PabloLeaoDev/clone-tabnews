298c6523d9ebdb117e7cc039d6b3eb1a30a169fd
Pablo Leão <151883726+PabloLeaoDev@users.noreply.github.com>
2026-07-18 14:18:35 -0300

    test: adds `dotenv-expand` in the `jest` setup

--- jest.setup.js
+++ jest.setup.js
@@ -1,1 +1,4 @@
-require("dotenv").config({ path: ".env.development" });
+const dotenv = require("dotenv"),
+  dotenvExpand = require("dotenv-expand"),
+  envConfig = dotenv.config({ path: ".env.development" });
+dotenvExpand.expand(envConfig);
